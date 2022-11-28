2ae578ee299d851af083d4ebd277b7cc97192221
Tom <thomasrosber@gmail.com>
2022-11-27 21:25:37 -0600

    new endpoint

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from src.views.users import retrieve
+from src.views.users import retrieve, store
 
 api = Blueprint('api', __name__,  url_prefix='/api/v1')
 
 api.route('/users', methods=['GET'])(retrieve)
+api.route('/users', methods=['POST'])(store)
--- src/views/users.py
+++ src/views/users.py
@@ -1,7 +1,29 @@
+from flask import request
+
 
 def retrieve():
     """
     Retrieve all users
     """
 
-    return {'result': 'hi'}
+    return {'result': 'retrieve'}
+
+
+def store():
+    """
+    Create user
+    """
+
+    content_type = request.headers.get('Content-Type')
+
+    if (content_type == 'application/json'):
+        json = request.get_json()
+
+        return {
+            'result': 'store',
+            'data': json
+            }, 201
+    else:
+        return {
+            'result': 'Content-Type not supported!'
+            }, 400
